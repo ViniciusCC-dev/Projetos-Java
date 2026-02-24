04beb6130ac595d8dad517ac6cfa0d5971a6743b
Vinicius Coelho <vvcglow.gg@gmail.com>
2026-02-23 21:25:17 -0300

    Exercicio feito

--- src/SomadorInfinito.java
+++ src/SomadorInfinito.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class SomadorInfinito {
     static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int valor;
+        int soma = 0;
+        do {
+            System.out.println("Digite um valor -(utilize 0 para encerrar): ");
+            valor = sc.nextInt();
+            soma += valor;
+        }while (valor != 0);{
+            System.out.print("O valor total é de: ");
+            System.out.println(soma);
+        }
 
+        sc.close();
     }
 }
