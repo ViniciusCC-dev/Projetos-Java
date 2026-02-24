28b8bf9f1c9b931fbb0423ac91759e68796199cb
Vinicius Coelho <vvcglow.gg@gmail.com>
2026-02-23 21:27:29 -0300

    adicao de media

--- src/SomadorInfinito.java
+++ src/SomadorInfinito.java
@@ -5,13 +5,18 @@ public class SomadorInfinito {
         Scanner sc = new Scanner(System.in);
         int valor;
         int soma = 0;
+        int contador = 0;
+        int media;
         do {
             System.out.println("Digite um valor -(utilize 0 para encerrar): ");
             valor = sc.nextInt();
             soma += valor;
+            contador++;
         }while (valor != 0);{
             System.out.print("O valor total é de: ");
             System.out.println(soma);
+            media = soma / contador;
+            System.out.println("A media desse valor é: " + media);
         }
 
         sc.close();
