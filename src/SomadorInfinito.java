7435a01a4f9fb98bd18230a4dd6d40c29b40d62f
Vinicius Coelho <vvcglow.gg@gmail.com>
2026-02-24 08:31:28 -0300

    Melhora na precisão da média e alternativa para caso nao tenha numeros para tirar a média

--- src/SomadorInfinito.java
+++ src/SomadorInfinito.java
@@ -1,23 +1,27 @@
 import java.util.Scanner;
 
 public class SomadorInfinito {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int valor;
         int soma = 0;
         int contador = 0;
-        int media;
         do {
+
             System.out.println("Digite um valor -(utilize 0 para encerrar): ");
             valor = sc.nextInt();
-            soma += valor;
-            contador++;
-        }while (valor != 0);{
-            System.out.print("O valor total é de: ");
-            System.out.println(soma);
-            media = soma / contador;
-            System.out.println("A media desse valor é: " + media);
-        }
+            if (valor != 0){
+                soma += valor;
+                contador++;
+            }
+        }while (valor != 0);
+            if(contador > 0 ) {
+                System.out.println("O valor total é de: " + soma);
+                double media = (double) soma / contador;
+                System.out.println("A media desse valor é: " + media);
+            }else {
+                System.out.println("Nenhum valor foi digitado");
+            }
 
         sc.close();
     }
